94295ce451a52c139de421ce8b0ed3c3f76d6770
Raven <theravenblu@gmail.com>
2018-01-23 15:12:40 +0100

    Added help text

--- server.lua
+++ server.lua
@@ -8,6 +8,4 @@ TriggerEvent('es:addGroupCommand', 'shapeshift', "mod", function(source, args, u
   end
 end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "You are not cool enough for this")
-end, {help = "Shapeshift into an animal"})
-
-
+end, {help = "Transform into another model", params = {{name = "userid", help = "The ID of the player"}, {name = "modelid", help = "Name of the model to transform into"}}})
